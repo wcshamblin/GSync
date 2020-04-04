2fb64d5e995e4d0a2b9a91c6765a0ad35f9390b9
wcshamblin <will@shamblin.org>
2020-04-04 11:06:07 -0400

    Removed folder hardcoding

--- backupbuffer.sh
+++ backupbuffer.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/zsh
-#tmux send-keys -t "Kiras Kingdom" "say §bBackup commencing..." ENTER
-#zip and copy current world to backup folder
-
-###### ONSITE BACKUPS ######
-#zip -r /home/mcserver/MCWorlds/ThePearl/`date "+%d-%m-%y-%H:%M"` /home/mcserver/ThePearl
-#while [[ "$(ls -l /home/mcserver/MCWorlds/ThePearl/ | wc -l)" -gt 7 ]];do
-#        rm "$(ls -1td /home/mcserver/MCWorlds/ThePearl/* | tail -n1)"
-#done
-############################
-
-###### OFFSITE BACKUPS ######
-rclone sync /home/mcserver/ThePearl GDrive:ThePearl -v --backup-dir GDrive:ThePearl`date "+.%d-%m-%y/%R"`/
+#!/usr/bin/bash
+LDIR='/path/to/dir/'    #local directory
+RDIR='GDrive:Directory' #rclone remote:directory
+rclone sync $LDIR $RDIR -v --backup-dir $RDIR`date "+.%d-%m-%y/%R"`/
